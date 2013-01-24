682d9ca44d4b2df18312bafc92a1a74653bfc070
Alex Samorukov <samm@os2.kiev.ua>
2013-01-24 18:47:13 +0100

    Add more platform-depended settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,8 +4,6 @@ USE_CAMERA_STUB := true
 -include vendor/alcatel/OT993D/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := 993D
 
 TARGET_PROVIDES_INIT_TARGET_RC := true
@@ -24,3 +22,18 @@ BOARD_HAS_NO_SELECT_BUTTON := false
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
+
+# some platform specific settings, not currently in use
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM: = msm7x27a
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CORTEX_CACHE_LINE_32 := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_QCOM_FM := true
